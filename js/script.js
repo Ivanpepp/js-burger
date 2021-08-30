a4f816c8898b24ff4c30008ec28b942f721f2be6
Ivanpepp <nastroivan3@gmail.com>
2021-08-31 00:44:07 +0200

    js-checkbox-section-complete

--- js/script.js
+++ js/script.js
@@ -10,3 +10,36 @@ Prima la logica in italiano e poi traduciamo in codice.
 Quando tutto funziona passate alla parte visiva lavorando al css.
 Buon lavoro! */
 
+let prezzoPanino;
+//prendo il nome del burger che metto nella inputbox
+let burgerName = document.getElementById('burger-name');
+//collego il bottone dell'html alla variabile bottone
+let bottone = document.getElementById('bottone');
+//qunado clicco il bottone deve accadere tutto
+bottone.addEventListener('click',
+function() {
+    if(burgerName.value.length === 0){
+        alert('inserisci il nome del panino');
+    }else{
+        //do il prezzo al panino
+        prezzoPanino = 9;
+        let aggiunte = document.getElementsByClassName('form-check-input'); // e vuoto? allora inserisci, ce qualcosa? allora prendi tutti le checkbox, fai una lista
+        for (let i=0; i < aggiunte.length; i++){
+            //scorro la lsita
+            if(aggiunte[i].checked == true && aggiunte[i] == document.getElementById('formaggio')){
+                prezzoPanino += 5;
+            }else if(aggiunte[i].checked == true && aggiunte[i] == document.getElementById('uova')){
+                prezzoPanino += 1000;
+            }else if(aggiunte[i].checked == true && aggiunte[i] == document.getElementById('pomodoro')){
+                prezzoPanino += 3;
+            }else if(aggiunte[i].checked == true && aggiunte[i] == document.getElementById('lattuga')){
+                prezzoPanino += 50;
+            }else if(aggiunte[i].checked == true && aggiunte[i] == document.getElementById('mostarda')){
+                prezzoPanino += 1;
+            }else if(aggiunte[i].checked == true && aggiunte[i] == document.getElementById('ketchup')){
+                prezzoPanino += 500000;
+            }
+        }
+        
+    }
+});
